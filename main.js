19be50c8e6f7eb97d1589e8bb2f7b53f9cf49dec
azurealstn <azurealstn@naver.com>
2020-11-01 22:26:25 +0900

    .vscode 제외

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const toggleBtn = document.querySelector('.navbar-toggleBtn');
 const menu = document.querySelector('.navbar-menu');
 const home = document.querySelector('.home-btn');
 const about = document.querySelector('.about-btn');
+const works = document.querySelector('.works-btn');
+const footer = document.querySelector('.footer-btn');
 
 toggleBtn.addEventListener('click', () => {
     menu.classList.toggle('active');
@@ -12,5 +14,13 @@ home.addEventListener('click', () => {
 })
 
 about.addEventListener('click', () => {
+    menu.classList.toggle('active');
+})
+
+works.addEventListener('click', () => {
+    menu.classList.toggle('active');
+})
+
+footer.addEventListener('click', () => {
     menu.classList.toggle('active');
 })
